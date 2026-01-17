bc7698b9dbd2caf47b3de3af1403f501b976ae82
alveesarker <asahanaz305@gmail.com>
2026-01-17 13:17:33 +0600

    solved largestSquareArea

---
+++ Medium/largestSquareArea.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestSquareArea(self, bottomLeft, topRight):
+        n = len(bottomLeft)
+        largest_len = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                left = max(bottomLeft[i][0], bottomLeft[j][0])
+                right = min(topRight[i][0], topRight[j][0])
+                bottom = max(bottomLeft[i][1], bottomLeft[j][1])
+                top = min(topRight[i][1], topRight[j][1])
+
+                width = right - left
+                height = top - bottom
+
+                if width <= 0 or height <= 0:
+                    continue
+
+                largest_len = max(largest_len, min(width, height))
+
+        return largest_len * largest_len
